3e12ecb2c02b421bec508eccb38465fb7c0a0f08
Christopher Hiller <boneskull@boneskull.com>
2024-07-28 14:24:29 -0700

    fix: bad types

--- src/paths.ts
+++ src/paths.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import {fileURLToPath} from 'url';
 
 export const RESOLVE_HOOKS_PATH = fileURLToPath(
+  // @ts-ignore
   new URL('./impvol-hooks.js', import.meta.url),
 );
 
+// @ts-ignore
 export const IMPVOL_URL = new URL('./impvol.js', import.meta.url).href;
