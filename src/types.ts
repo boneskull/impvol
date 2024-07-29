108c3989269fdce36ef89960ac9983378cf0ad1b
Christopher Hiller <boneskull@boneskull.com>
2024-07-29 11:40:09 -0700

    chore: cleanup cruft, fix tsconfig

--- src/types.ts
+++ src/types.ts
@@ -1,37 +1,3 @@
-// eslint-disable-next-line n/no-missing-import
-import {type SnapshotNode} from 'memfs/lib/snapshot/types.js';
-// eslint-disable-next-line n/no-missing-import
-import {type JsonUint8Array} from 'memfs/lib/snapshot/json.js';
-
-declare const tag: unique symbol;
-
-export type Tagged<K, T> = K & {[tag]: T};
-
-export type ImpVolId = Tagged<string, 'ImpVolId'>;
-
-export interface BaseImpVolEvent {
-  type: string;
-  id: ImpVolId;
-  impVolId: ImpVolId;
-}
-
-export interface ImpVolUpdateEvent extends BaseImpVolEvent {
-  type: 'UPDATE';
-  json: JsonUint8Array<SnapshotNode>;
-}
-
-export interface ImpVolClearEvent extends BaseImpVolEvent {
-  type: 'CLEAR';
-}
-
-export interface ImpVolAckEvent extends BaseImpVolEvent {
-  type: 'ACK';
-}
-
-export type ImpVolHookEvent = ImpVolAckEvent;
-
-export type ImpVolEvent = ImpVolUpdateEvent | ImpVolClearEvent;
-
 export type ImpVolInitData = {
   tmp: string;
   sab: SharedArrayBuffer;
